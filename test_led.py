c97703991ac298bf6166ac7d574a9fbd346dff36
Benjamin Dostie <bendostie@outlook.com>
2021-09-22 10:17:47 -0400

    adds test for led pin 18

---
+++ test_led.py
@@ -0,0 +1,12 @@
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+led = 18
+button = 19
+GPIO.setup(button,GPIO.IN)
+GPIO.setup(led,GPIO.OUT)
+GPIO.output(led, True)
+print(GPIO.input(button))
+time.sleep(3000)
+GPIO.cleanup()
